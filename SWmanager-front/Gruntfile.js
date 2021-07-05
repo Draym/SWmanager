04a467043f903ba00fdb6f4afaaf478bb3738abe
Draym <kevin.draym@gmail.com>
2021-07-05 16:57:20 +0800

    try with github token

--- SWmanager-front/Gruntfile.js
+++ SWmanager-front/Gruntfile.js
@@ -38,7 +38,11 @@ module.exports = function (grunt) {
 
     'gh-pages': {
       options: {
-        base: 'dist'
+        base: 'dist',
+        user: {
+          name: 'Draym',
+          email: 'kevin.draym@gmail.com'
+        }
       },
       src: ['**']
     },
