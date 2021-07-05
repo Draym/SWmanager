3bbbe04c4bd5fde82c44eebf6806bfc7a9f9d088
Kevin Andres <kevin.andres@stey.com>
2021-07-05 16:21:05 +0800

    only run test once

--- SWmanager-front/test/karma.conf.js
+++ SWmanager-front/test/karma.conf.js
@@ -6,7 +6,7 @@ module.exports = function(config) {
 
   config.set({
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
 
     // base path, that will be used to resolve files and exclude
     basePath: '../',
@@ -75,7 +75,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false,
+    singleRun: true,
 
     colors: true,
 
